ce239cf0f89f5f541d046b2d1e3d257b6323f502
Alejandro Martínez <amrkratos@hotmail.com>
2022-07-03 14:43:14 -0500

    Primera version de mi contador en react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
+/* REact libraries */
+import React, {useState} from 'react';
+/* react styles */
+import './App.css'
+
+
+const App = ()=>{
+  const [count, setCount] = useState(0);
+  const [increment, setInc] = useState(1);
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="carta">
+        <div class="cont">
+          <h1>Contador</h1>
+          <h2>{count}</h2>
+          <button onClick={()=>setCount(count+increment)}>Suma</button>
+          <button onClick={()=>setCount(count-increment)}>Resta</button>
+          <button onClick={()=>setCount(0)}>Reset</button>
+        </div>
+        
+        <div class="inc">
+          <h1>Incremento</h1>
+          <h2>{increment}</h2>
+          <button onClick={()=>setInc(increment +1)}>Suma</button>
+          <button onClick={()=>setInc(increment - 1)}>Resta</button>
+          <button onClick={()=>setInc(0)}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
